7e6c0d1e9779457aa76d23b9bebf941e740dad7d
Varun <varunvadda99@gmail.com>
2022-10-17 23:42:34 +0530

    added component and homepage

---
+++ client/src/components/shared/Button/Button.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = ({ text }) => {
+  return (
+    <button className={styles.button}>
+      <span>{text}</span>
+      <img src="/images/arrow-forward.png" alt="arrow" />
+    </button>
+  );
+};
+
+export default Button;
--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -1,9 +1,39 @@
 /** @format */
 
 import React from "react";
-import "./Home.module.css";
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+import Card from "../../components/shared/Card/Card";
+import Button from "../../components/shared/Button/Button";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const signinLiskStyle = {
+    color: "#0077ff",
+    fontWeight: "bold",
+    textDecoration: "none",
+    marginLeft: "10px",
+  };
+
+  return (
+    <div className={styles.cardWrapper}>
+      <Card title="Welcome to Kosha!" icon="logo">
+        <p className={styles.text}>
+          We’re working hard to get Kosha ready for everyone! While we wrap up
+          the finishing youches, we’re adding people gradually to make sure
+          nothing breaks
+        </p>
+        <div>
+          <Button text="Get your userName" />
+        </div>
+        <div className={styles.signinWrapper}>
+          <span className={styles.hasInvite}>Have an invite text?</span>
+          <Link style={signinLiskStyle} to="/login">
+            Sign in
+          </Link>
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default HomePage;
