ac6cd9f8ad95ed701c0e5140546b7a5511ead09a
Varun <varunvadda99@gmail.com>
2022-11-02 00:11:56 +0530

    Working on activate pages

--- client/src/pages/Steps/StepName/StepName.jsx
+++ client/src/pages/Steps/StepName/StepName.jsx
@@ -1,10 +1,31 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./StepName.module.css";
+import Card from "../../../components/shared/Card/Card";
+import Button from "../../../components/shared/Button/Button";
+import Textinput from "../../../components/shared/Textinput/Textinput";
 
-const StepName = () => {
-  return <div>StepName</div>;
+const StepName = ({ onNext }) => {
+  const [name, setName] = useState("");
+
+  const submit = async () => {};
+
+  return (
+    <>
+      <Card title="EWhat's your full name?" icon="goggle-emoji">
+        <Textinput value={name} onChange={(e) => setName(e.target.value)} />
+        <div>
+          <p className={styles.bottomParagrap}>
+            People use real names at Kosha
+          </p>
+          <div className={styles.actionButtonWrapper}>
+            <Button text="Next" onClick={submit} />
+          </div>
+        </div>
+      </Card>
+    </>
+  );
 };
 
 export default StepName;
