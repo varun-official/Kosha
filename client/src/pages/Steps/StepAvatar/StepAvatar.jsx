df67bace27f78b2802c72345e71fe93441e38d82
Varun <varunvadda99@gmail.com>
2022-11-04 00:23:13 +0530

    completed UI of stepAvathar page

--- client/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ client/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -5,12 +5,24 @@ import styles from "./StepAvatar.module.css";
 import Card from "../../../components/shared/Card/Card";
 import Button from "../../../components/shared/Button/Button";
 import { useDispatch, useSelector } from "react-redux";
+import { setAvathar } from "../../../store/ActivateSlice";
 
 const StepAvatar = () => {
   const { name } = useSelector((state) => state.activate);
   const [image, setImage] = useState("/images/monkey-avatar.png");
+  const dispatch = useDispatch();
 
   const submit = async () => {};
+  const captureImage = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImage(reader.result);
+      dispatch(setAvathar(reader.result));
+    };
+  };
+
   return (
     <>
       <div className={styles.cardWrapp}>
@@ -19,6 +31,17 @@ const StepAvatar = () => {
             <p className={styles.subHedaing}>How's this photo?</p>
             <div className={styles.avatarWrapper}>
               <img className={styles.avatarImage} src={image} alt="avatar" />
+            </div>
+            <div className={styles.avatarInputWrapper}>
+              <input
+                onChange={captureImage}
+                id="avatarInput"
+                type="file"
+                className={styles.avatarInput}
+              />
+              <label className={styles.avatarLabel} htmlFor="avatarInput">
+                choose a different photo
+              </label>
             </div>
             <div className={styles.actionButtonWrapper}>
               <Button text="Next" onClick={submit} />
