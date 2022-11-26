3b216ac012aa1bd023742e3b05fcaab0b03be869
Varun <varunvadda99@gmail.com>
2022-11-26 17:08:08 +0530

    Completed roomHeader bar

--- client/src/pages/Rooms/Rooms.jsx
+++ client/src/pages/Rooms/Rooms.jsx
@@ -4,7 +4,27 @@ import React from "react";
 import styles from "./Rooms.module.css";
 
 const Rooms = () => {
-  return <div>Rooms</div>;
+  return (
+    <>
+      <div className="container">
+        <div className={styles.roomHeader}>
+          <div className={styles.left}>
+            <span className={styles.heading}>All voice rooms</span>
+            <div className={styles.searchBox}>
+              <img src="/images/search-icon.png" alt="search" />
+              <input type="text" className={styles.searchInput} />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <button className={styles.startButton}>
+              <img src="/images/add-room-icon.png" alt="add-room" />
+              <span>Start a room</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Rooms;
