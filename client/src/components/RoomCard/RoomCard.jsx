91445511ebf922e52bbd80c4076c527dbc478b9f
Varun <varunvadda99@gmail.com>
2022-12-12 16:12:04 +0530

    Bug fix

--- client/src/components/RoomCard/RoomCard.jsx
+++ client/src/components/RoomCard/RoomCard.jsx
@@ -6,22 +6,21 @@ import { useNavigate } from "react-router-dom";
 
 const RoomCard = ({ room }) => {
   const navigate = useNavigate();
-
   return (
     <div onClick={() => navigate(`/room/${room.id}`)} className={styles.card}>
       <h3 className={styles.topic}>{room.topic}</h3>
       <div
         className={`${styles.spekers} ${
-          room.spekers.length === 1 ? styles.oneSpeaker : ""
+          room.speakers.length === 1 ? styles.oneSpeaker : ""
         }`}
       >
         <div className={styles.avatars}>
-          {room?.spekers.map((speaker) => (
+          {room.speakers?.map((speaker) => (
             <img key={speaker.id} src={speaker.avatar} alt="speaker" />
           ))}
         </div>
         <div className={styles.names}>
-          {room?.spekers.map((speaker) => (
+          {room.speakers?.map((speaker) => (
             <div key={speaker.id} className={styles.nameWraper}>
               <span>{speaker.name}</span>
               <img src="/images/chat-bubble.png" alt="chat-bubble" />
@@ -30,7 +29,7 @@ const RoomCard = ({ room }) => {
         </div>
       </div>
       <div className={styles.participants}>
-        <span>{room.participants}</span>
+        <span>{room.participants ? room.participants : 0}</span>
         <img src="/images/user-icon.png" alt="user-icon" />
       </div>
     </div>
--- client/src/pages/Rooms/Rooms.jsx
+++ client/src/pages/Rooms/Rooms.jsx
@@ -8,83 +8,83 @@ import { getAllRooms } from "../../http/index";
 import Loader from "../../components/shared/Loader/Loader";
 const Rooms = () => {
   const [showModal, setShowModal] = useState(false);
-  // const [rooms, setRooms] = useState([]);
+  const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
     const featchRooms = async () => {
       // setRooms([]);
       const { data } = await getAllRooms();
       console.log(data);
-      // setRooms(data);
+      setRooms(data);
     };
     featchRooms();
   }, []);
-  const rooms = [
-    {
-      id: 1,
-      topic: "room1",
-      spekers: [
-        {
-          id: 1,
-          name: "varun",
-          avatar: "/images/monkey-avatar.png",
-        },
-        {
-          id: 2,
-          name: "vivek",
-          avatar: "/images/monkey-avatar.png",
-        },
-      ],
-      participants: 20,
-    },
-    {
-      id: 2,
-      topic: "room2",
-      spekers: [
-        {
-          id: 1,
-          name: "varun",
-          avatar: "/images/monkey-avatar.png",
-        },
-      ],
-      participants: 200,
-    },
-    {
-      id: 3,
-      topic: "room3",
-      spekers: [
-        {
-          id: 1,
-          name: "varun",
-          avatar: "/images/monkey-avatar.png",
-        },
-        {
-          id: 2,
-          name: "vivek",
-          avatar: "/images/monkey-avatar.png",
-        },
-      ],
-      participants: 200,
-    },
-    {
-      id: 4,
-      topic: "room4",
-      spekers: [
-        {
-          id: 1,
-          name: "varun",
-          avatar: "/images/monkey-avatar.png",
-        },
-      ],
-      participants: 10,
-    },
-  ];
+  // const rooms = [
+  //   {
+  //     id: 1,
+  //     topic: "room1",
+  //     spekers: [
+  //       {
+  //         id: 1,
+  //         name: "varun",
+  //         avatar: "/images/monkey-avatar.png",
+  //       },
+  //       {
+  //         id: 2,
+  //         name: "vivek",
+  //         avatar: "/images/monkey-avatar.png",
+  //       },
+  //     ],
+  //     participants: 20,
+  //   },
+  //   {
+  //     id: 2,
+  //     topic: "room2",
+  //     spekers: [
+  //       {
+  //         id: 1,
+  //         name: "varun",
+  //         avatar: "/images/monkey-avatar.png",
+  //       },
+  //     ],
+  //     participants: 200,
+  //   },
+  //   {
+  //     id: 3,
+  //     topic: "room3",
+  //     spekers: [
+  //       {
+  //         id: 1,
+  //         name: "varun",
+  //         avatar: "/images/monkey-avatar.png",
+  //       },
+  //       {
+  //         id: 2,
+  //         name: "vivek",
+  //         avatar: "/images/monkey-avatar.png",
+  //       },
+  //     ],
+  //     participants: 200,
+  //   },
+  //   {
+  //     id: 4,
+  //     topic: "room4",
+  //     spekers: [
+  //       {
+  //         id: 1,
+  //         name: "varun",
+  //         avatar: "/images/monkey-avatar.png",
+  //       },
+  //     ],
+  //     participants: 10,
+  //   },
+  // ];
 
   const openModal = () => {
     setShowModal(true);
   };
 
-  // if (rooms == null) return <Loader />;
+  if (!rooms) return <Loader />;
 
   return (
     <>
@@ -105,9 +105,7 @@ const Rooms = () => {
           </div>
         </div>
         <div className={styles.roomList}>
-          {rooms.map((room) => (
-            <RoomCard room={room} key={room.id} />
-          ))}
+          {rooms && rooms.map((room) => <RoomCard room={room} key={room.id} />)}
         </div>
       </div>
       {showModal && <AddRoomModal onClose={() => setShowModal(false)} />}
