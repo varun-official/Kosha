4fcc29485868b708308f7c0b645076d4e318e572
Varun <varunvadda99@gmail.com>
2022-12-24 00:38:57 +0530

    Working on RTC connection

--- client/src/hooks/useWebRTC.js
+++ client/src/hooks/useWebRTC.js
@@ -1,18 +1,22 @@
 /** @format */
 
+import { useRef } from "react";
 import { useStateWithCallback } from "./useStateWithCallback";
+const users = [
+  {
+    id: 1,
+    name: "Varun",
+  },
+  {
+    id: 2,
+    name: "Vivek",
+  },
+];
+export const useWebRTC = (roomId, user) => {
+  const [clients, setClients] = useStateWithCallback(users);
+  const audioElements = useRef({});
+  const connections = useRef({});
+  const localMediaStreams = useRef(null);
 
-export const useWebRTC = () => {
-  const [clients, setClients] = useStateWithCallback([
-    {
-      id: 1,
-      name: "Varun",
-    },
-    {
-      id: 2,
-      name: "Vivek",
-    },
-  ])
-
-  return {clients}
+  return { clients };
 };
--- client/src/pages/Room/Room.jsx
+++ client/src/pages/Room/Room.jsx
@@ -1,11 +1,15 @@
 /** @format */
 
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { useWebRTC } from "../../hooks/useWebRTC";
 import styles from "./Room.module.css";
 
 const Room = () => {
-  const { clients } = useWebRTC();
+  const {id: roomId}  = useParams();
+  const user = useSelector(state => state.auth.user)
+  const { clients } = useWebRTC(roomId,user);
   return (
     <div>
       <h1>All connected clients</h1>
